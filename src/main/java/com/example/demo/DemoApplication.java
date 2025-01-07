c62e2e77e113c5862fe038099d5c83223118e42d
Contreras Clavijo Danny <danny.contrerasclavijo@groupensi.com>
2025-01-07 13:56:09 -0500

    moving project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		Logger logger = Logger.getLogger(DemoApplication.class.getName());
+
+		List<String> texts = new ArrayList<>();
+        texts.add("Hello World :)!");
+		logger.log(Level.INFO, () -> String.valueOf(texts));
+	}
+}
